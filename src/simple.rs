63dd44e12a8f3f0c0962cd1559beac13c6ab1548
uff20xd <donut121008@gmail.com>
2026-01-21 09:34:58 +0100

    k

---
+++ src/simple.rs
@@ -0,0 +1,66 @@
+use std::collections::*;
+use std::default;
+#[derive(Default)]
+pub struct StackMachine {
+    stack: VecDeque<usize>,
+    instructions: Vec<Instruction>,
+    instruction_pointer: usize,
+}
+
+#[derive(Copy, Clone)]
+pub enum Instruction {
+    Add, Sub, 
+    Push(usize),
+    Eq,
+    If,
+    Jump,
+    Stop,
+}
+
+impl StackMachine {
+    pub fn new(instructions: Vec<Instruction>) -> Self {
+        Self {
+            ..default::Default::default()
+        }
+    }
+    pub fn exec(&mut self) -> () {
+        while self.instruction_pointer < self.instructions.len() {
+            match self.instructions[self.instruction_pointer]{
+                Instruction::Add => {
+                    let a = self.stack.pop_back().unwrap();
+                    let b = self.stack.pop_back().unwrap();
+                    self.stack.push_back(a + b);
+                },
+                Instruction::Sub => {
+                    let a = self.stack.pop_back().unwrap();
+                    let b = self.stack.pop_back().unwrap();
+                    self.stack.push_back(a - b);
+                },
+                Instruction::Push(val) => {
+                    self.stack.push_back(val);
+                },
+                Instruction::Eq => {
+                    let a = self.stack.pop_back().unwrap();
+                    let b = self.stack.pop_back().unwrap();
+                    self.stack.push_back((a == b) as usize);
+                },
+                Instruction::If => {
+                    let a = self.stack.pop_back().unwrap() != 0;
+                    if !a { self.instruction_pointer += 1;}
+                },
+                Instruction::Jump => {
+                    let a = self.stack.pop_back().unwrap();
+                    self.instruction_pointer = a;
+                },
+                Instruction::Stop => {
+                    return;
+                }
+            }
+            self.instruction_pointer += 1;
+        }
+    }
+
+    #[inline(always)]
+    fn execute_instruction(&mut self, instruction: Instruction) {
+    }
+}
