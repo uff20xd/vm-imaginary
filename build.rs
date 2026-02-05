2cc816b45e6b9ac17b4d9b8d39388157b0c26561
uff20xd <donut121008@gmail.com>
2026-02-05 13:17:49 +0100

    k

--- build.rs
+++ build.rs
@@ -6,10 +6,21 @@ const RUST_FLAGS: &[&str] = &["--edition=2024"];
 fn main() {
     let _redoxri = Redoxri::new(&[""]);
 
-    let vm = Mcule::new("vm-imaginary", "./vm-imaginary")
+    let out = Mcule::new("output", "out/")
+        .add_step(&["mkdir", "out"])
+        .compile();
+
+    let vm = Mcule::new("vm-imaginary", "./out/vm-imaginary")
         .with(&["src/main.rs".into()])
         .add_step(&["rustc", "src/main.rs", "-o", "$out"])
         .with_flags(RUST_FLAGS)
         .compile()
         .run();
+
+    // let vm_test1 = Mcule::new("vm-imaginary-test1", "./vm-imaginary")
+    //     .with(&["src/main.rs".into()])
+    //     .add_step(&["rustc", "src/main.rs", "-o", "$out"])
+    //     .with_flags(RUST_FLAGS)
+    //     .compile()
+    //     .run();
 }
